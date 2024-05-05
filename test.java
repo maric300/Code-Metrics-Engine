adf30b567f2ce182f9ce817da6983aba4d41e9bf
maric300 <branko.maric300@gmail.com>
2024-05-05 19:04:13 +0200

    fix: fixed package errors

--- test.java
+++ test.java
@@ -1,30 +1,34 @@
-public void fun1() {
-	for(int i = 1; i < 5; i++) {
-		Console.WriteLine(i)
+public class test {
+	public void fun1() {
+		for(int i = 1; i < 5; i++) {
+
+		}
+
+		for(int i = 1; i < 5; i++) {
+
+		}
 	}
 
-	for(int i = 1; i < 5; i++) {
-		Console.WriteLine(i);
+	public void Afun2() {
+		int i = 1;
+		for(i = 1; i < 5; i++) {
+
+		}
+
+		if(i == 5) {
+
+		}
+
+		fun3();
 	}
+
+
+	public void fun3() {
+		for(int i = 1; i < 5; i++) {
+
+		}
+	}
+
 }
 
-public void Afun2() {
-
-	for(int i = 1; i < 5; i++) {
-		Console.WriteLine(i);
-	}
-	
-	if(i > 5) {
-	Console.WriteLine(i);
-	}
-
-	fun3();
-}
-
-
-public void fun3() {
-	for(int i = 1; i < 5; i++) {
-		Console.WriteLine(i)
-	}
-}
 
